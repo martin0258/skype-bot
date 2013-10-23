f0c749f1fa21e65247a76281f0ec3fd886d5327c
martin.ku <martin79831@gmail.com>
2013-10-23 22:33:02 +0800

    Added logging function to track behaviors.

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
   description = "========== Use Skype Bot to send messages automatically =========="
   parser = argparse.ArgumentParser(description=description)
   parser.add_argument('-c', '--config', default='example.cfg', help='config file. Use [example.cfg] by default')
+  parser.add_argument('-l', '--logfile', default='running.log', help='log file. Use [running.log] by default')
   parser.add_argument('-v', '--version', action='version', version='Skype Bot 0.1')
   args = parser.parse_args()
 
@@ -38,10 +39,23 @@ if __name__ == "__main__":
   message           = config.get('Skype','message')
   triggerDay        = config.get('Automation', 'weekday')
 
+  # Set up log file
+  executionTime = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+  logFileFullPath = "%s/%s" % (folder, args.logfile)
+  logFile = open(logFileFullPath, 'a+')
+  logFile.write('[%s] ' % executionTime)
+  logMessage = 'Message not sent. (for some reasons)'
+
   if str(datetime.date.today().weekday()) == triggerDay:
     osPlatform = platform.system()
     skype = Skype4Py.Skype(Transport=Skype4PyInterface) if osPlatform=="Linux" else Skype4Py.Skype()
     skype.Attach()
-    skype.CreateChatUsingBlob(blob).SendMessage(message)
+    chat = skype.CreateChatUsingBlob(blob)
+    chat.SendMessage(message)
+    logMessage = 'Message sent.'
 
+  logFile.write('%s\n' % logMessage)
+  logFile.flush()
+  os.fsync(logFile.fileno())
+  logFile.close()
   sys.exit(0)
